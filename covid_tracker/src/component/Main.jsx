8779a572c9bc0c44c86116b2816e235dc857ca25
pradumn <pradumnpatel18@gmail.com>
2021-07-28 10:22:50 +0530

    adding table

--- covid_tracker/src/component/Main.jsx
+++ covid_tracker/src/component/Main.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Main.css";
 import Menu from "../assets/Menu1.png";
 import Covid from "../assets/corona.jpg";
+import Table from "./Table";
 
 function Main() {
   const [styles, setstyles] = useState("-100rem");
+  const [TableData, setTableData] = useState([]);
+  const [value, setvalue] = useState(window.innerWidth);
+  window.onresize = displayWindowSize;
+  window.onload = displayWindowSize;
+  function displayWindowSize() {
+    setvalue(window.innerWidth);
+  }
   function clicked() {
     styles == "-100rem" || styles == !0 ? setstyles(0) : setstyles("-100rem");
   }
+
+  const sortData = (data) => {
+    let sortedData = [...data];
+    sortedData.sort((a, b) => {
+      if (a.cases > b.cases) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    return sortedData;
+  };
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          let sortedData = sortData(data);
+          setTableData(sortedData);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <>
       <nav>
@@ -37,24 +71,27 @@ function Main() {
         </div>
       </nav>
       <div className="center">
-        <div className="info">
-          <div className="info-1">
-            <h5>Get Well Soon,Earth!</h5>
+        <div className="container">
+          <div className="info">
+            <div className="info-1">
+              <h5>Get Well Soon,Earth!</h5>
+            </div>
+            <h1>
+              Let's Help <br /> Recover The Earth,
+              <br /> Starts From Us!{" "}
+            </h1>
+            <p>
+              The outbreak of the coronavirus issue <br />
+              or called Covid-19 makes the Earth grieve.
+            </p>
+            <div className="button">
+              <div className="btn">Learn More</div>
+              <div className="btn">Watch Video</div>
+            </div>
           </div>
-          <h1>
-            Let's Help <br /> Recover The Earth,
-            <br /> Starts From Us!{" "}
-          </h1>
-          <p>
-            The outbreak of the coronavirus issue <br />
-            or called Covid-19 makes the Earth grieve.
-          </p>
-          <div className="button">
-            <div className="btn">Learn More</div>
-            <div className="btn">Watch Video</div>
-          </div>
+          <img className="img" src={Covid} alt="covid" />
         </div>
-        <img className="img" src={Covid} alt="covid" />
+        <Table countries={TableData} />
       </div>
     </>
   );
---
+++ covid_tracker/src/component/Table.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import numeral from "numeral";
+import "./Table.css";
+
+function Table({ countries }) {
+  return (
+    <>
+      <div className="data">
+        <h3 style={{ textAlign: "center" }}>Live Cases by Country</h3>
+        <div className="table">
+          <tr
+            style={{
+              height: "3rem",
+            }}
+          >
+            <th>Country</th>
+            <th>Flag</th>
+            <th>Cases</th>
+          </tr>
+          {countries.map((country) => (
+            <tr>
+              <td>{country.country}</td>
+              <td>
+                <img src={country.countryInfo.flag} alt="flag" />{" "}
+              </td>
+              <td>
+                <strong>{numeral(country.cases).format("0,0")}</strong>
+              </td>
+            </tr>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Table;
